48c67fb30f04b245e0e09d4d729e98013a6d8149
Rohit <r4rohit002@gmail.com>
2020-01-23 17:23:59 +0530

    Added IDS in the layout

--- app/src/main/java/edu/asu/msse/rsingh92/assignment1/MainActivity.java
+++ app/src/main/java/edu/asu/msse/rsingh92/assignment1/MainActivity.java
@@ -11,14 +11,27 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView name, description, category, addressTitle, addressStreet, elevation, latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        description = findViewById(R.id.description);
+        category = findViewById(R.id.category);
+        addressTitle = findViewById(R.id.addressTitle);
+        addressStreet = findViewById(R.id.addressStreet);
+        elevation = findViewById(R.id.elevation);
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
 
     }
 }
