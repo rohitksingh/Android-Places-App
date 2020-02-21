ebaad391367fc6e8494286fc353972ddaf31c8b1
Rohit <r4rohit002@gmail.com>
2020-02-21 13:23:55 -0700

    Added Header information removed unused imports

--- app/src/main/java/edu/asu/msse/rsingh92/assignment1/RPC/AsyncCollectionConnect.java
+++ app/src/main/java/edu/asu/msse/rsingh92/assignment1/RPC/AsyncCollectionConnect.java
@@ -16,6 +16,25 @@ import edu.asu.msse.rsingh92.assignment1.models.PlaceDescription;
 import edu.asu.msse.rsingh92.assignment1.utilities.AppUtility;
 import edu.asu.msse.rsingh92.assignment1.utilities.PlaceLibrary;
 
+/*
+ * Copyright 2020 Rohit Kumar Singh,
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Rohit Kumar Singh rsingh92@asu.edu
+ *
+ * @version February 2016
+ */
 public class AsyncCollectionConnect extends AsyncTask<RPCMethodMetadata, Integer, RPCMethodMetadata> {
 
     private static List<PlaceDescription> allPlaces;
--- app/src/main/java/edu/asu/msse/rsingh92/assignment1/activities/SplashActivity.java
+++ app/src/main/java/edu/asu/msse/rsingh92/assignment1/activities/SplashActivity.java
@@ -1,6 +1,5 @@
 package edu.asu.msse.rsingh92.assignment1.activities;
 
-import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,10 +8,29 @@ import com.airbnb.lottie.LottieAnimationView;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.asu.msse.rsingh92.assignment1.R;
 import edu.asu.msse.rsingh92.assignment1.RPC.AsyncCollectionConnect;
-import edu.asu.msse.rsingh92.assignment1.RPC.DeletePlaceAsyncTask;
 import edu.asu.msse.rsingh92.assignment1.RPC.RPCMethodMetadata;
 import edu.asu.msse.rsingh92.assignment1.callbacks.RPCCallback;
 
+
+/*
+ * Copyright 2020 Rohit Kumar Singh,
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Rohit Kumar Singh rsingh92@asu.edu
+ *
+ * @version February 2016
+ */
 public class SplashActivity extends AppCompatActivity implements RPCCallback {
 
     LottieAnimationView lottieAnimationView;
